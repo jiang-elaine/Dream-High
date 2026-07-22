6bd33a266768706429f85608e2664714ab77af63
jiang-elaine <jiangelaine4@gmail.com>
2026-07-22 14:12:58 -0400

    Add new resource for TCGA Breast Cancer data

--- profile.js
+++ profile.js
@@ -48,6 +48,10 @@ const studentProfile = {
     {
       title: "Exploring TCGA Breast Cancer Clinical Data",
       url: "https://rpubs.com/elainejiang/1447596"
+    },
+    {
+      title: "Heatmaps with TCGA Breast Cancer Gene Expression Data",
+      url: "https://rpubs.com/elainejiang/1448573"
     }
   ],
 
